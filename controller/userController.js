e2caef8e15e679ed0cc6c88316319d7736ed7a3e
Yanms18 <158842637+Yanms18@users.noreply.github.com>
2025-04-17 15:38:57 +0100

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -20,7 +20,6 @@ const getAllUsers = async (req, res) => {
     const users = await User.findAll();
     res.status(200).json(users);
   } catch (error) {
-    console.error('Error fetching users:', error);
     res.status(500).json({ message: 'Failed to retrieve users' });
   }
 };
