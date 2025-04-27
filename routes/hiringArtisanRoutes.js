7ac72501256a6b11f0bea8a8259fa5bb0a2a2d26
yanms18 <jejeoluwayanmife@gmail.com>
2025-04-27 17:51:20 +0100

    Fix endpoint for retrieving hiring requests to use '/allhire'

--- routes/hiringArtisanRoutes.js
+++ routes/hiringArtisanRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const { createHiringRequest, getHiringRequests } = require('../controller/hiringArtisanController');
-
 const router = express.Router();
 
 // Route to create a new hiring request
 router.post('/hire', createHiringRequest);
 
 // Route to get all hiring requests
-router.get('/hire', getHiringRequests);
+router.get('/allhire', getHiringRequests);
 
 module.exports = router;
